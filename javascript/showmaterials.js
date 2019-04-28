462cc98c5eba3f963d2521c12255bdae23fbf001
EzequielMussambe <chipilicafamily@gmail.com>
2019-04-28 03:18:35 -0400

    update_2

--- javascript/showmaterials.js
+++ javascript/showmaterials.js
@@ -17,3 +17,21 @@ if (x.style.display === "none") {
     x.style.display = "none";
 }
 }
+
+function anaFunction() {
+    var x = document.getElementById('analysis');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    }
+function ReFunction() {
+        var x = document.getElementById('Results');
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+        }
+    
